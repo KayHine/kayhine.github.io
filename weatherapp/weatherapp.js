a97235e3cf9782666e0fc05096517c904a2802d4
KayHine <volibur@gmail.com>
2017-01-24 21:34:00 -0500

    Add files via upload

--- weatherapp/weatherapp.js
+++ weatherapp/weatherapp.js
@@ -1,1 +1,17 @@
+var currLoc = document.getElementByID("currLoc");
 
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    currLoc.innerHTML = "Geolocation disabled";
+  }
+}
+
+function showPosition(pos) {
+  currLoc.innerHTML = pos.coords;
+}
+
+$(document).ready(function() {
+  $("#getloc").on("click", getLocation);
+});
